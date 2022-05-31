7399464535364bf3b4d167ef0542249522c3ae3c
codewithashith <codewithashith@gmail.com>
2022-05-31 19:29:59 +0530

    Initial commit

---
+++ src/test/java/com/codewithashith/demoRESTapi/DemoResTapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codewithashith.demoRESTapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoResTapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
